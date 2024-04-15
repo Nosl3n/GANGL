fe581b8e3719088b8adf40c107c29ad6cf4f19e9
Nosl3n <nelson.broyer@gmail.com>
2024-04-15 09:46:41 -0500

    grupos aleatorios con el nuevo modelo, pruebas

--- GANGL_v01/Copy_of_grupos_aleatorios.m
+++ GANGL_v01/Copy_of_grupos_aleatorios.m
@@ -3,8 +3,8 @@ clc;
 clear all;
 close all;
 % Distribución 4 personas ordenadas de forma cuadrada.
-li=5;%limite de coordenadas, maximo 5.
-limvec=2; %limite del numero de personas.
+li=8;%limite de coordenadas, maximo 5.
+limvec=5; %limite del numero de personas.
 n = randi([2, limvec]);% Generar un número aleatorio de elementos para el vector (n)
 x = li * rand(1, n); % Valores aleatorios
 y = li * rand(1, n); % Valores aleatorios 
